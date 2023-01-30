d775adf088915b1403809c32d281fa675d23ffc2
CenkCamkiran <cenkcamkiran@gmail.com>
2023-01-30 16:55:02 +0300

    command updated

--- AWS-Serverless-StorageApplication.Commands/UploadObjectCommand.cs
+++ AWS-Serverless-StorageApplication.Commands/UploadObjectCommand.cs
@@ -9,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace AWS_Serverless_StorageApplication.Commands
 {
-    public class UploadObjectCommand: IRequest<FileDetails>
+    public class UploadObjectCommand : IRequest<FileDetails>
     {
+        public long SizeInBytes { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; }
+        public Dictionary<string, string> MetaData { get; set; } = new Dictionary<string, string>();
+
+        public UploadObjectCommand(long sizeInBytes, string name, string contentType, DateTime createDate, Dictionary<string, string> metaData)
+        {
+            SizeInBytes = sizeInBytes;
+            Name = name;
+            ContentType = contentType;
+            CreateDate = createDate;
+            MetaData = metaData;
+        }
     }
 }
