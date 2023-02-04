9cd1297e298965e9109283c54e4269520e1e64fa
Cenk Camkıran <34924560+CenkCamkiran@users.noreply.github.com>
2023-02-04 19:15:47 +0300

    request validation fixed

--- AWS-Serverless-StorageApplication.Middleware/ErrorHandlerMiddleware.cs
+++ AWS-Serverless-StorageApplication.Middleware/ErrorHandlerMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using AWS_Serverless_StorageApplication.Models;
+﻿using AWS_Serverless_StorageApplication.Helpers;
+using AWS_Serverless_StorageApplication.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
@@ -22,12 +23,25 @@ namespace AWS_Serverless_StorageApplication.Middleware
             {
                 await _next(httpContext);
             }
-            catch (Exception exception)
+            catch (StorageApplicationException exception)
             {
                 var response = httpContext.Response;
                 response.ContentType = MediaTypeNames.Application.Json;
 
                 StorageApplicationError error = JsonConvert.DeserializeObject<StorageApplicationError>(exception.Message.ToString());
+                response.StatusCode = error.ResponseCode;
+                response.Headers.Add(HttpResponseHeader.ContentType.ToString(), MediaTypeNames.Application.Json);
+
+                await response.WriteAsync(JsonConvert.SerializeObject(error));
+            }
+            catch (Exception exception)
+            {
+                var response = httpContext.Response;
+                response.ContentType = MediaTypeNames.Application.Json;
+
+                StorageApplicationError error = new StorageApplicationError();
+                error.Message = exception.Message;
+                error.ResponseCode = (int)HttpStatusCode.InternalServerError;
                 response.StatusCode = error.ResponseCode;
                 response.Headers.Add(HttpResponseHeader.ContentType.ToString(), MediaTypeNames.Application.Json);
 
