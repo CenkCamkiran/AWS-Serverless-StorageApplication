774c71c256b853aec7571cc4115fc00531f3de3c
Cenk Camkıran <34924560+CenkCamkiran@users.noreply.github.com>
2023-02-17 14:03:09 +0300

    comment line

--- AWS-Serverless-StorageApplication/Startup.cs
+++ AWS-Serverless-StorageApplication/Startup.cs
@@ -21,6 +21,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container
     public void ConfigureServices(IServiceCollection services)
     {
+        //Assembilies
         //Assembly.GetAssembly(typeof(CreateObjectCommand));
         //Assembly.GetAssembly(typeof(DeleteObjectCommand));
         //Assembly.GetAssembly(typeof(CreateBucketCommand));
